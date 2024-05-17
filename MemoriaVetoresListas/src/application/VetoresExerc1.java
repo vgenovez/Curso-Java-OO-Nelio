d89e0edfb0780325d8af607706437caebfb4902e
Vitor Genovez <vitorgenovez@ibm.com>
2024-05-17 15:14:20 -0300

    Exercicios Vetores 1 e 2

---
+++ MemoriaVetoresListas/src/application/VetoresExerc1.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class VetoresExerc1 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos numeros voce vai digitar? ");
+		int n = sc.nextInt();
+		
+		int[] vet = new int[n];
+		
+		for (int i=0; i<vet.length; i++) {
+			System.out.print("Digite um numero: ");
+			vet[i] = sc.nextInt();
+		}
+		
+		System.out.println("Numeros negativos: ");
+		for (int i=0; i<vet.length; i++) {
+			if (vet[i] < 0) {
+				System.out.println(vet[i]);
+			}
+		}
+
+	}
+
+}
