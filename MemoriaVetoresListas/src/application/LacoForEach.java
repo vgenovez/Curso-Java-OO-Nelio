b7bbb1114f40b8b59497f6eeefa35f2a42f204fd
vitorgenovez <vitorgenovez@msn.com>
2024-05-20 21:00:52 -0300

    Aula lacoforeach

---
+++ MemoriaVetoresListas/src/application/LacoForEach.java
@@ -0,0 +1,17 @@
+package application;
+
+public class LacoForEach {
+
+	public static void main(String[] args) {
+		
+		String[] vect = new String [] {"Maria", "Bob", "Alex"};
+		
+		for (int i = 0; i < vect.length; i++) {
+			System.out.println(vect[i]);
+		}
+		
+		for (String obj : vect) {
+			System.out.println(obj);
+		}
+	}
+}
