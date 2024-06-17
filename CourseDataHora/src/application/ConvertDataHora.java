5d9a42fe08dab4473818444b39f3d252741f415c
Vitor Genovez <vitorgenovez@ibm.com>
2024-06-17 08:57:38 -0300

    Conversao de horas

--- CourseDataHora/src/application/ConvertDataHora.java
+++ CourseDataHora/src/application/ConvertDataHora.java
@@ -13,6 +13,25 @@ public class ConvertDataHora {
 		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
 		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 		
+		LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+		LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+		LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+		LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+		System.out.println("r1 = " + r1);
+		System.out.println("r2 = " + r2);
+		System.out.println("r1 = " + r3);
+		System.out.println("r2 = " + r4);
+		
+		System.out.println("d04 dia = " + d04.getDayOfMonth());
+		System.out.println("d04 dia = " + d04.getMonthValue());
+		System.out.println("d04 dia = " + d04.getYear());
+
+		System.out.println("d05 hora = " + d05.getHour());
+		System.out.println("d05 hora = " + d05.getMinute());		
+		//for ( String s : ZoneId.getAvailableZoneIds()) {
+		//	System.out.println(s); //Nomes que podem usar para especificar o fusohorario
+		//}
+		
 		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
@@ -30,6 +49,8 @@ public class ConvertDataHora {
 		System.out.println("d06 = " + fmt3.format(d06));
 		System.out.println("d06 = " + fmt5.format(d06));
 		System.out.println("d06 = " + d06.toString());
+		
+
 	}
 
 }
